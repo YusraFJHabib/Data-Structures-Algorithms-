a34ef90f1ec1d240ae79a8536697e8c6e3a9bbf1
Hypno <126251597+YusraFJHabib@users.noreply.github.com>
2024-12-15 14:30:29 -0500

    Add files via upload
    Lab 7

--- Main.java
+++ Main.java
@@ -1,39 +1,46 @@
 class Main {
   public static void main(String[] args) {
+    String[] vertices = {
+        "Liberal Arts",
+        "Student Services",
+        "Health Careers & Sciences",
+        "Health Technologies Center",
+        "Recreation Center",
+        "Technology Learning Center",
+        "Business & Technology",
+        "Theatre"
+    };
 
-    // Step 1 - Create a BST tree object called lab5Tree
-    BST lab5Tree = new BST();
+   int[][] edges = {
+        {0, 1}, // Liberal Arts to Student Services
+        {1, 2}, // Student Services to Health Careers & Sciences
+        {2, 3}, // Health Careers & Sciences to  Health Technologies Center
+        {3, 4}, // Health Technologies Center to Recreation Center
+        {4, 5}, // Recreation Center to Technology Learning Center
+        {5, 6}, // Technology Learning Center to Business & Technology
+        {6, 7}  // Business & Technology to Theatre
+        //I could have {7,0} from Theatre to liberal arts I guess but it doesnt make sense
+    };
+  
 
-    // Step 2 - Insert the following values: 13, 22, 36, 5, 48, 17, 39, 2, 26, 40, 29, 34, 10
-    int[] values = {13, 22, 36, 5, 48, 17, 39, 2, 26, 40, 29, 34, 10};
-        for (int value : values) {
-            lab5Tree.insert(value);
-        }
+    Graph<String> graph = new UnweightedGraph<>(vertices, edges);
+    UnweightedGraph<String>.SearchTree dfs = graph.dfs(graph.getIndex("Business & Technology")); // Get a dfs starting at the Business and Technology Building. Change this is you called it something different in your vertices!
 
-    // Step 3 - Delete the value 17
-    lab5Tree.delete(17);
+    java.util.List<Integer> searchOrders = dfs.getSearchOrder();
+    System.out.println(dfs.getNumberOfVerticesFound() + " vertices are searched in this DFS order:");
+    for (int i = 0; i < searchOrders.size(); i++)
+      System.out.print(graph.getVertex(searchOrders.get(i)) + " ");
+    System.out.println();
 
-    // Step 4 - Traverse and output the values using inorder (sorted)
-    System.out.println("Inorder traversal (sorted): " + lab5Tree.inorder());
+    for (int i = 0; i < searchOrders.size(); i++)
+      if (dfs.getParent(i) != -1)
+        System.out.println("parent of " + graph.getVertex(i) +
+            " is " + graph.getVertex(dfs.getParent(i)));
 
-    // Step 5 - Traverse and output the values using postorder
-    System.out.println("Postorder traversal: " + lab5Tree.postorder());
+    dfs.printPath(graph.getIndex("Health Technologies Center"));
+    dfs.printPath(graph.getIndex("Student Services"));
+    dfs.printPath(graph.getIndex("Recreation Center"));
 
-    // Step 6 - Traverse and output the values using preorder
-    System.out.println("Preorder traversal: " + lab5Tree.preorder());
-
-
-    // Step 7 - Display the result of a search for the value 36
-    System.out.println("Search for 36: " + (lab5Tree.search(36) ? "Found" : "Not Found"));
-
-    // Step 8 - Display the result of a search for the value 37
-    System.out.println("Search for 37: " + (lab5Tree.search(37) ? "Found" : "Not Found"));
-
-    // Step 9 - Using the path() method, display the path from the root to 2
-    System.out.println("Path to 2: " + lab5Tree.path(2));
-
-    // Step 10 - Display the path from the root to 34
-    System.out.println("Path to 34: " + lab5Tree.path(34));
-
+    dfs.printTree();
   }
 }
